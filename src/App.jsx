dbdf24a4d9465157de3a7c65f1a9697d556d5bfb
CODonnie <ordudesigns@gmail.com>
2025-07-16 07:38:56 -0700

    pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import Body from "./components/Body";
+import { Routes, Route } from "react-router-dom";
+import Welcome from './pages/Welcome';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Project from './pages/Project';
+import NotFound from './components/NotFound';
 
 export default function App() {
 
-  return <div>
-    <Body />
-  </div>
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="Project" element={<Project />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
 }
