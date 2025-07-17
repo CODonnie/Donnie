188adad888f2ef6ac68aa289e2701b91380e0f36
CODonnie <ordudesigns@gmail.com>
2025-07-17 09:24:43 -0700

    project display

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,10 +1,18 @@
 import { useParams } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../context/Appcontext';
 
 const Project = () => {
   const { id } = useParams();
   const { project } = useContext(AppContext);
+  const [ showProject, setShowProject ] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() =>{
+      setShowProject(true);
+    }, 3000)
+    
+  }, []);
 
   const selectedProject = project.find(item => String(item.id) === id);
 
@@ -13,9 +21,13 @@ const Project = () => {
   }
 
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold mb-4">{selectedProject.title}</h1>
-      <p className="mb-2 text-lg">{selectedProject.info}</p>
+    <div className="">
+      <div className={`bg-green-400 w-full ${showProject ? "animate-div-cover" : "animate-div-remove"} transition-all duration-300`}></div>
+      <div>
+        <h1 onClick={()=> setShowProject(true)} className="text-4xl font-bold mb-4">{selectedProject.title}</h1>
+        <p className="mb-2 text-lg">{selectedProject.info}</p>
+      </div>
+
       {/* Add more project details here */}
     </div>
   );
